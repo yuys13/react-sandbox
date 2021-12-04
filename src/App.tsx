1010ca4da5f00ba5cb92f1cbdca80006c52e62d7
yuys13 <yuys13@users.noreply.github.com>
2021-12-04 16:40:53 +0900

    Fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 
-const App: React.VFC = function () {
+const App: React.VFC = function app() {
   return (
     <div className="App">
       <header className="App-header">
