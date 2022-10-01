0b9ed50b4986da6dd547db4dc0d0b28168015918
yuys13 <yuys13@users.noreply.github.com>
2022-10-01 17:25:00 +0900

    Use react18
    TODO:resolutions is workaround to pass storyshot tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 
-const App: React.VFC = function app() {
+const App = function app() {
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
