c72514b511ea8ee101e749bf12407d53861f94a9
yuys13 <yuys13@users.noreply.github.com>
2023-02-24 10:40:36 +0900

    refactor: use function instead of const variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 
-const App: React.VFC = function app() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
