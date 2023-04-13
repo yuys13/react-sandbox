22ce6ee77395e9730b90e583937bf5bec9713fa6
yuys13 <yuys13@users.noreply.github.com>
2023-04-13 20:46:06 +0900

    feat: add sample of react hooks (#670)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import logo from './logo.svg'
 
-function App() {
+const App: React.VFC = function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>You clicked {count} times</p>
+        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+          Click me
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
